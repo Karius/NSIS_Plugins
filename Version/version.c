ea68fe25a1a5dea1415fbc63dd7bedd91ee30335
Karius <kittarius@gmail.com>
2013-01-04 21:51:27 +0800

    Version first commit.

---
+++ Version/version.c
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+// GetVersionEx
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms724451%28v=vs.85%29.aspx
+
+// VerifyVersionInfo
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms725492%28v=vs.85%29.aspx
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms725491%28v=vs.85%29.aspx
+// 
+// OSVERSIONINFOEX
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms724833%28v=vs.85%29.aspx
+
